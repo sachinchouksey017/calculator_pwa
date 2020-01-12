948445224ef91bcd6803b084107e4e8a7aa31ff5
1301mohit <mohitkr1301@gmail.com>
2020-01-12 18:32:31 +0530

    just checking pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,110 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  subText = ''; // The text that should appear in the sub-display
+  mainText = ''; // The text that should appear in the main display
+  operand1: number; // The first operand
+  operand2: number; // The second operand
+  operator = ''; // The operator
+  calculationString = '';
+  // This is the string that denotes the operation being performed
+  answered = false;
+  // A flag to check whether the solution has been processed
+  operatorSet = false; // You'll see how this is used soon
+  deferredPrompt: any;
+  showButton = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        console.log('choice is ',choiceResult);
+        
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
+  }
+  pressKey(key: string) {
+
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      const lastKey = this.mainText[this.mainText.length - 1];
+      console.log('last ', this.mainText);
+
+      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+') {
+        this.operatorSet = true;
+        console.log(key, lastKey);
+
+      }
+      if ((this.operatorSet) || (this.mainText === '')) {
+        return;
+      }
+      this.operand1 = parseFloat(this.mainText);
+      this.operator = key;
+      this.operatorSet = true;
+    }
+    if (this.mainText.length === 10) {
+      return;
+    }
+    console.log(this.mainText);
+
+    this.mainText += key;
+  }
+  allClear() {
+    this.subText = '';
+    // this.mainText = '';
+  }
+  getAnswer() {
+    this.calculationString = this.mainText;
+    this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 / this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = this.mainText.substr(0, 9);
+      }
+    } else if (this.operator === 'x') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 * this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else if (this.operator === '-') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 - this.operand2).toString();
+      this.subText = this.calculationString;
+    } else if (this.operator === '+') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 + this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else {
+      this.subText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+  }
 }
